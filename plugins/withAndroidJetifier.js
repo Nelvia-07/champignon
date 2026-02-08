16c42b46dd333935617788562ba98baa9e3b13b4
Iris <ch_yj07@163.com>
2026-02-08 10:15:56 +0800

    Apply aggressive Gradle resolution strategy and cleanup Android project

--- plugins/withAndroidJetifier.js
+++ plugins/withAndroidJetifier.js
@@ -1,43 +1,45 @@
 const { withGradleProperties, withProjectBuildGradle } = require('@expo/config-plugins');
 
 const withAndroidJetifier = (config) => {
-    // 1. Set gradle properties
-    config = withGradleProperties(config, (config) => {
-        config.modResults.push({
-            type: 'property',
-            key: 'android.enableJetifier',
-            value: 'true',
-        });
-        config.modResults.push({
-            type: 'property',
-            key: 'android.useAndroidX',
-            value: 'true',
-        });
-        return config;
+  // 1. Set gradle properties
+  config = withGradleProperties(config, (config) => {
+    config.modResults.push({
+      type: 'property',
+      key: 'android.enableJetifier',
+      value: 'true',
     });
+    config.modResults.push({
+      type: 'property',
+      key: 'android.useAndroidX',
+      value: 'true',
+    });
+    return config;
+  });
 
-    // 2. Aggressively exclude support-compat to fix Duplicate classes error
-    config = withProjectBuildGradle(config, (config) => {
-        if (config.modResults.contents.includes('exclude group: "com.android.support"')) {
-            return config;
-        }
+  // 2. Aggressively exclude support-compat to fix Duplicate classes error
+  config = withProjectBuildGradle(config, (config) => {
+    if (config.modResults.contents.includes('exclude group: "com.android.support"')) {
+      return config;
+    }
 
-        const exclusionBlock = `
+    const exclusionBlock = `
 allprojects {
     configurations.all {
         resolutionStrategy {
             force 'androidx.core:core:1.13.1'
+            force 'androidx.appcompat:appcompat:1.6.1'
+            force 'androidx.fragment:fragment:1.7.1'
+            force 'androidx.annotation:annotation:1.8.0'
         }
-        exclude group: "com.android.support", module: "support-compat"
-        exclude group: "com.android.support", module: "support-v4"
+        exclude group: "com.android.support"
     }
 }
 `;
-        config.modResults.contents += exclusionBlock;
-        return config;
-    });
-
+    config.modResults.contents += exclusionBlock;
     return config;
+  });
+
+  return config;
 };
 
 module.exports = withAndroidJetifier;
